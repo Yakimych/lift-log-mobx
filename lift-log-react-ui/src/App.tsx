d3708289929e4e3b319b81fab18fd573cf5a02f3
Kyrylo Yakymenko <yakimych@gmail.com>
2019-04-22 11:53:22 +0200

    Add React UI

---
+++ lift-log-react-ui/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App: React.FunctionComponent = () => (
+  <div className="App">
+    <header className="app-header">LiftLog</header>
+    <div className="content">Placeholder</div>
+  </div>
+);
+
+export default App;
